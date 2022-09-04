0c63b6ce5a4a0ffbae7916efad58024e92d2da8e
Cholete99 <choleteunion@gmail.com>
2022-09-03 22:20:16 -0600

    add test set

--- src/utils/testSet.ts
+++ src/utils/testSet.ts
@@ -1,4 +1,4 @@
-const full = [
+export const reg = [
   {
     amount: "1/2",
     unit: "cup",
@@ -27,7 +27,7 @@ const full = [
     amount: "0",
     unit: "",
     name: "eggs",
-    id: "5",
+    id: "5134",
   },
   {
     amount: "1",
@@ -67,4 +67,203 @@ const full = [
   },
 ];
 
-export default full;
+export const many = [
+  {
+    amount: "1/2",
+    unit: "cup",
+    name: "water",
+    id: "1",
+  },
+  {
+    amount: "5",
+    unit: "tbsp",
+    name: "sugar",
+    id: "2",
+  },
+  {
+    amount: "2.5",
+    unit: "tsp",
+    name: "vanilla",
+    id: "3",
+  },
+  {
+    amount: "7/2",
+    unit: "cups",
+    name: "milk",
+    id: "4",
+  },
+  {
+    amount: "0",
+    unit: "",
+    name: "eggs",
+    id: "5213890",
+  },
+  {
+    amount: "1",
+    unit: "tsp",
+    name: "brown sugar",
+    id: "6",
+  },
+  {
+    amount: "100/100",
+    unit: "ml",
+    name: "water",
+    id: "7",
+  },
+  {
+    amount: "1/3",
+    unit: "",
+    name: "brown eggs",
+    id: "8",
+  },
+  {
+    amount: "0.9",
+    unit: "grams",
+    name: "butter",
+    id: "9",
+  },
+  {
+    amount: "2 3/4",
+    unit: "cup",
+    name: "water",
+    id: "10",
+  },
+  {
+    amount: "1 1/3",
+    unit: "cup",
+    name: "water",
+    id: "11",
+  },
+  {
+    amount: "1/2",
+    unit: "cup",
+    name: "water",
+    id: "12",
+  },
+  {
+    amount: "5",
+    unit: "tbsp",
+    name: "sugar",
+    id: "22",
+  },
+  {
+    amount: "2.5",
+    unit: "tsp",
+    name: "vanilla",
+    id: "32",
+  },
+  {
+    amount: "7/2",
+    unit: "cups",
+    name: "milk",
+    id: "42",
+  },
+  {
+    amount: "0",
+    unit: "",
+    name: "eggs",
+    id: "5182390",
+  },
+  {
+    amount: "1",
+    unit: "tsp",
+    name: "brown sugar",
+    id: "62",
+  },
+  {
+    amount: "100/100",
+    unit: "ml",
+    name: "water",
+    id: "72",
+  },
+  {
+    amount: "1/3",
+    unit: "",
+    name: "brown eggs",
+    id: "82",
+  },
+  {
+    amount: "0.9",
+    unit: "grams",
+    name: "butter",
+    id: "92",
+  },
+  {
+    amount: "2 3/4",
+    unit: "cup",
+    name: "water",
+    id: "102",
+  },
+  {
+    amount: "1 1/3",
+    unit: "cup",
+    name: "water",
+    id: "112",
+  },
+  {
+    amount: "1/2",
+    unit: "cup",
+    name: "water",
+    id: "133",
+  },
+  {
+    amount: "5",
+    unit: "tbsp",
+    name: "sugar",
+    id: "233",
+  },
+  {
+    amount: "2.5",
+    unit: "tsp",
+    name: "vanilla",
+    id: "333",
+  },
+  {
+    amount: "7/2",
+    unit: "cups",
+    name: "milk",
+    id: "433",
+  },
+  {
+    amount: "0",
+    unit: "",
+    name: "eggs",
+    id: "522",
+  },
+  {
+    amount: "1",
+    unit: "tsp",
+    name: "brown sugar",
+    id: "622",
+  },
+  {
+    amount: "100/100",
+    unit: "ml",
+    name: "water",
+    id: "722",
+  },
+  {
+    amount: "1/3",
+    unit: "",
+    name: "brown eggs",
+    id: "822",
+  },
+  {
+    amount: "0.9",
+    unit: "grams",
+    name: "butter",
+    id: "922",
+  },
+  {
+    amount: "2 3/4",
+    unit: "cup",
+    name: "water",
+    id: "1011",
+  },
+  {
+    amount: "1 1/3",
+    unit: "cup",
+    name: "water",
+    id: "1111",
+  },
+];
